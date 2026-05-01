7d4144162164efe065de56ecfcc5373a5f315237
Vivek Yadav <vivek57yadav@gmail.com>
2026-05-01 17:52:45 +0530

    xxx

--- tcrs-frontend/app/login/page.tsx
+++ tcrs-frontend/app/login/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 export const dynamic = 'force-dynamic';
-
+export const fetchCache = 'force-no-store';
 import { useState, useEffect, Suspense } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import API from '@/lib/api';
