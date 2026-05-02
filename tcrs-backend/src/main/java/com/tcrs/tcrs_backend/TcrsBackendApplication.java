e6c7fbfc7cad65cb0cd667ce3d8d958261d6d964
Vivek Yadav <vivek57yadav@gmail.com>
2026-05-02 18:32:34 +0530

    Debug: Add startup log to print MongoDB URI

--- tcrs-backend/src/main/java/com/tcrs/tcrs_backend/TcrsBackendApplication.java
+++ tcrs-backend/src/main/java/com/tcrs/tcrs_backend/TcrsBackendApplication.java
@@ -2,12 +2,26 @@ package com.tcrs.tcrs_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
 public class TcrsBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TcrsBackendApplication.class, args);
-	}
+    @Value("${spring.data.mongodb.uri:NOT_FOUND}")
+    private String mongoUri;
 
+    public static void main(String[] args) {
+        SpringApplication.run(TcrsBackendApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner logMongoUri() {
+        return args -> {
+            System.out.println("=========================================================");
+            System.out.println("CRITICAL DEBUG: Loaded MongoDB URI is: " + mongoUri);
+            System.out.println("=========================================================");
+        };
+    }
 }
